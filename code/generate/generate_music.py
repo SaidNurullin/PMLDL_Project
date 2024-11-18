a0e744cbe6c800dfab8da80e57c0b17fdd52cf84
Said Nurullin <s.nurullin@innopolis.university>
2024-11-18 18:30:57 +0300

    new model created

--- code/generate/generate_music.py
+++ code/generate/generate_music.py
@@ -4,89 +4,94 @@ import os
 import pickle
 
 
-def load_h5_files(folder_path):
-    h5_files = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".h5"):
-            file_path = os.path.join(folder_path, filename)
-            h5_files.append(file_path)
-
-    return h5_files
-
-
-def generate_music(instrument_network_input, encoders, n_vocabs, models, scalers, instruments, indices):
-
+def generate_music(network_input, note_encoder, instrument_encoder, n_vocab, model, starts_scaler, durations_scaler, velocity_scaler, instruments):
     # Create MIDI data
     midi = pretty_midi.PrettyMIDI()
     instrument_track = pretty_midi.Instrument(program=0)
 
     num_notes_to_generate = 100
 
-    lens = []
+    start = np.random.randint(0, len(network_input) - 1)
 
+    # start = 0
 
-    for i in range(len(instrument_network_input)):
-        lens.append(len(instrument_network_input[i]))
+    # Generate music
+    int_to_note = dict((number, note) for number, note in enumerate(note_encoder.classes_))
+    int_to_instrument = dict((number, instrument) for number, instrument in enumerate(instrument_encoder.classes_))
+    pattern = network_input[start]
 
-    start = np.random.randint(0, min(lens) - 1)
+    pattern = [int_to_note[int(value * n_vocab)] for value in pattern]
+    prediction_output = []
+    prediction_starts = []
+    prediction_durations = []
+    prediction_instruments = []
+    prediction_velocities = []
 
+    for note_index in range(num_notes_to_generate):
+        # Prepare input sequence
+        input_seq = [note_encoder.transform([n])[0] for n in pattern]
+        input_seq = np.reshape(input_seq, (1, len(input_seq), 1))
+        input_seq = input_seq / float(n_vocab)
 
-    for j in range(len(instruments)):
-
-        if not(j in indices):
-            continue
-        # Generate music
-        int_to_note = dict((number, note) for number, note in enumerate(encoders[j].classes_))
-        pattern = instrument_network_input[j][start]
-
-        pattern = [int_to_note[int(value * n_vocabs[j])] for value in pattern]
-        prediction_output = []
-        prediction_starts = []
-        prediction_durations = []
-
-        for note_index in range(num_notes_to_generate):
-            # Prepare input sequence
-            input_seq = [encoders[j].transform([n])[0] for n in pattern]
-            input_seq = np.reshape(input_seq, (1, len(input_seq), 1))
-            input_seq = input_seq / float(n_vocabs[j])
-
-            # Predict
-            prediction = models[j].predict(input_seq, verbose=0)
-            note_prediction = prediction[0]
-            start_time_prediction = prediction[1][0][0]
-            duration_prediction = prediction[2][0][0]
+        # Predict
+        prediction = model.predict(input_seq, verbose=0)
+        note_prediction = prediction[0]
+        start_time_prediction = prediction[1][0][0]
+        duration_prediction = prediction[2][0][0]
+        instrument_prediction = prediction[3]
+        velocity_prediction = prediction[4][0][0]
 
     #         # Apply diversity (optional)
     #         note_prediction = np.log(note_prediction) / 1.0
     #         exp_preds = np.exp(note_prediction)
     #         note_prediction = exp_preds / np.sum(exp_preds)
 
-            # Decode and store predictions
-            index = np.argmax(note_prediction)
-            result = int_to_note[index]
-            prediction_output.append(result)
-            prediction_starts.append(scalers[j].inverse_transform([[start_time_prediction]]).flatten()[0])
-            prediction_durations.append(duration_prediction)
+        # Decode and store predictions
+        index = np.argmax(note_prediction)
+        result = int_to_note[index]
+        prediction_output.append(result)
+        prediction_starts.append(starts_scaler.inverse_transform([[start_time_prediction]]).flatten()[0])
+        prediction_durations.append(durations_scaler.inverse_transform([[duration_prediction]]).flatten()[0])
+        prediction_velocities.append(velocity_scaler.inverse_transform([[velocity_prediction]]).flatten()[0])
 
-            # Append to the pattern
-            pattern.append(result)
-            # pattern = pattern[1:]
+        index = np.argmax(instrument_prediction)
+        instrument = int_to_instrument[index]
+        prediction_instruments.append(instrument)
 
-        note_start = 0
-        for i, note_name in enumerate(prediction_output):
-            if i == 0:
-                note_start = prediction_starts[i]
-            else:
-                note_start = prediction_starts[i] + note_start
-            note_end = note_start + prediction_durations[i]
+        # Append to the pattern
+        pattern.append(result)
+        pattern = pattern[1:]
 
-            note = pretty_midi.Note(velocity=100, pitch=int(note_name), start=note_start, end=note_end)
-            instrument_track.notes.append(note)
+    note_start = 0
+    # print(prediction_instruments[:10])
+    # print(prediction_velocities[:10])
+    # print(prediction_starts[:10])
+    # print(prediction_durations[:10])
 
-            # Assign instrument to note
-            instrument_track.name = instruments[j]
+    for i, note_name in enumerate(prediction_output):
 
-            midi.instruments.append(instrument_track)
+        if i == 1:
+            note_start = prediction_starts[i]
+        else:
+            note_start = prediction_starts[i] + note_start
+        note_end = note_start + prediction_durations[i]
+
+        if(not(prediction_instruments[i] in instruments)):
+            continue
+
+        # print(note_start)
+
+        note_velocity = int(prediction_velocities[i])
+
+        note = pretty_midi.Note(velocity=note_velocity, pitch=int(note_name), start=note_start, end=note_end)
+        instrument_track.notes.append(note)
+
+        # Assign instrument to note
+        instrument_track.name = prediction_instruments[i]
+
+        # print(prediction_instruments[i])
+
+        midi.instruments.append(instrument_track)
 
     midi.write('generated_music.mid')
 
@@ -94,19 +99,14 @@ def generate_music(instrument_network_input, encoders, n_vocabs, models, scalers
 current_dir = os.getcwd()
 parent_dir = os.path.dirname(current_dir)
 grandparent_dir = os.path.dirname(parent_dir)
-models_dir = os.path.join(grandparent_dir, 'models')
+models_dir = os.path.join(grandparent_dir, 'model')
 encoders_dir = os.path.join(grandparent_dir, 'encoders')
 scalers_dir = os.path.join(grandparent_dir, 'scalers')
-inputs_dir = os.path.join(grandparent_dir, 'inputs')
-n_vocabs_dir = os.path.join(grandparent_dir, 'n_vocabs')
+inputs_dir = os.path.join(grandparent_dir, 'input')
+n_vocabs_dir = os.path.join(grandparent_dir, 'n_vocab')
 instruments_dir = os.path.join(grandparent_dir, 'instruments')
 
-models = []
-encoders = []
-scalers = []
 
-instrument_network_input = []
-n_vocabs = []
 instruments = []
 
 
@@ -115,6 +115,7 @@ def load_pkl(directory, array):
 
         if filename.endswith(".pkl"):
             path = os.path.join(directory, filename)
+            # print(path)
 
             with open(path, 'rb') as f:
                 file = pickle.load(f)
@@ -125,24 +126,31 @@ def load_np(directory, array):
     for filename in os.listdir(directory):
         if filename.endswith(".npy"):
             path = os.path.join(directory, filename)
+            # print(path)
             array.append(np.load(path))
 
 
-load_pkl(models_dir, models)
+model = []
+encoders = []
+scalers = []
+network_input = []
+n_vocab = []
+instruments = []
+
+load_pkl(models_dir, model)
 load_pkl(encoders_dir, encoders)
 load_pkl(scalers_dir, scalers)
 
-load_np(inputs_dir, instrument_network_input)
-load_np(n_vocabs_dir, n_vocabs)
+load_np(inputs_dir, network_input)
+load_np(n_vocabs_dir, n_vocab)
 load_np(instruments_dir, instruments)
 
-n_vocabs = n_vocabs[0]
+n_vocab = n_vocab[0]
 instruments = instruments[0]
+model = model[0]
+network_input = network_input[0]
 
-indices = []
+# instruments = instruments[:1]
+# print(instruments)
 
-indices.append(1)
-indices.append(2)
-
-
-generate_music(instrument_network_input, encoders, n_vocabs, models, scalers, instruments, indices)
+generate_music(network_input, encoders[1], encoders[0], n_vocab, model, scalers[1], scalers[0], scalers[2], instruments)
